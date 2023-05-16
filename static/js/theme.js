e7eb5443e65630d78eab5c4b9617462b22d32884
Joselito Guardado <jaguardado123@gmail.com>
2023-05-16 18:25:54 -0500

    Updated toggle-switch

--- static/js/theme.js
+++ static/js/theme.js
@@ -4,52 +4,68 @@
 
 */
 function themeSwitch() {
-    let dark = document.getElementById("toggle").checked;
-    console.log(dark);
 
-    let background = title = text = card = theme = image = "";
+    // Get boolean value of input checkbox.
+    let dark = document.getElementById("toggle").checked;
+
+    // Declare color variables then initialize.
+    let background = title = text = card = theme = image = line = "";
     if (dark) {
-        /* ==== Light Mode ==== */
+        /* ------------------------------------
+        # Light Mode 
+        ------------------------------------*/
         background = "var(--background-light)";
         title = "var(--title-light)";
         text = "var(--text-light)";
         card = "var(--card-light)";
         theme = "var(--secondary)";
         image = "url(static/img/coffeecupblue-bg.jpg)";
+        line = "#0f70b5";
     }
     else {
-        /* ==== Dark Mode ==== */
+        /*-----------------------------------
+        # Dark Mode
+        -----------------------------------*/
         background = "var(--background-dark)";
         title = "var(--title-dark)";
         text = "var(--text-dark)";
         card = "var(--card-dark)";
         theme = "var(--primary)";
         image = "url(static/img/coffeecup-bg.jpg)";
+        line = "#f0551e";
     }
 
-    /* ======= Change hero background image ======== */
+    /*-----------------------------------
+    # Hero Image
+    -----------------------------------*/
     document.getElementsByClassName("bg-image")[0].style.backgroundImage = image;
 
-    /* ======= Change body background and text color ===== */
+    /*-----------------------------------
+    # Background and Text
+    -----------------------------------*/
     let body = document.getElementsByTagName("body")[0];
     body.style.background = background;
     body.style.color = text;
 
-    /* ====== Change background of card boxes ======= */
+    /*-----------------------------------
+    # Card Boxes
+    -----------------------------------*/
     document.getElementsByClassName("box-shadow-full")[0].style.background = card;
     let work_box = document.getElementsByClassName("work-box");
     for (let i = 0; i < work_box.length; i++) {
         work_box[i].style.background = card;
     }
 
-    /* ====== Change color of titles and subtitles ======== */
+    /*----------------------------------
+    # Titles and Subtitles
+    ----------------------------------*/
     let w_titles = document.getElementsByClassName("w-title");
     for (let i = 0; i < w_titles.length; i++) {
         w_titles[i].style.color = text;
     }
     let title_left = document.getElementsByClassName("title-left")[0];
     title_left.style.color = title;
-    title_left.style.setProperty("--primary", "#0f70b5"); /* <-- pseudo-element */
+    title_left.style.setProperty("--primary", line); /* <-- pseudo-element */
     document.getElementsByClassName("skill-mf")[0].getElementsByTagName("span")[0].style.color = text;
     let subtitle_a = document.getElementsByClassName("subtitle-a");
     for (let i = 0; i < subtitle_a.length; i++) {
@@ -60,7 +76,9 @@ function themeSwitch() {
         title_s[i].style.color = title;
     }
 
-    /* ======= Change theme of overlay, lines, and buttons background ======= */
+    /*-----------------------------------
+    # Overlay, Lines, and Buttons
+    -----------------------------------*/
     document.getElementsByClassName("overlay-mf")[0].style.background = theme;
     let line_mf = document.getElementsByClassName("line-mf");
     for (let i = 0; i < line_mf.length; i++) {
@@ -68,7 +86,9 @@ function themeSwitch() {
     }
     document.getElementsByClassName("back-to-top")[0].style.background = theme;
 
-    /* ====== Portfolio category and links ======= */
+    /*---------------------------------
+    # Categories and Links
+    ---------------------------------*/
     let w_category = document.getElementsByClassName("w-ctegory");
     for (let i = 0; i < w_category.length; i++) {
         w_category[i].style.color = theme;
@@ -78,7 +98,9 @@ function themeSwitch() {
         links[i].getElementsByTagName("a")[0].style.color = theme;
     }
 
-    /* ====== Change About social icons background ===== */
+    /*--------------------------------
+    # Social Icons in About Me
+    --------------------------------*/
     let social_button = document.getElementsByClassName("personal_social")[0].getElementsByTagName("a");
     for (let i = 0; i < social_button.length; i++) {
         social_button[i].style.background = theme;
