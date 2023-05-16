a4a82df6c7df03941fe564be4fcc81df962321b6
Joselito Guardado <jaguardado123@gmail.com>
2023-05-15 19:56:42 -0500

    Updated portfolio content

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,419 +1,87 @@
-;(function($){
-    "use strict"
-	
-	
-	var nav_offset_top = $('header').height() + 50; 
-    /*-------------------------------------------------------------------------------
-	  Navbar 
-	-------------------------------------------------------------------------------*/
+/*
 
-	//* Navbar Fixed  
-    function navbarFixed(){
-        if ( $('.header_area').length ){ 
-            $(window).scroll(function() {
-                var scroll = $(window).scrollTop();   
-                if (scroll >= nav_offset_top ) {
-                    $(".header_area").addClass("navbar_fixed");
-                } else {
-                    $(".header_area").removeClass("navbar_fixed");
-                }
-            });
-        };
-    };
-    navbarFixed();
-	
-	
-	/*----------------------------------------------------*/
-    /*  Parallax Effect js
-    /*----------------------------------------------------*/
-	function parallaxEffect() {
-    	$('.bg-parallax').parallax();
-	}
-	parallaxEffect();
-	
-	
-//	$('.courses_area').imagesLoaded(function(){
-//        $('.courses_inner').isotope({ 
-//            layoutMode: 'masonry',
-//			percentPosition: true,
-//			masonry: {
-//				columnWidth: 1,
-//			}
-//        })
-//    });
-	
-	
-	
-	
-	/*----------------------------------------------------*/
-    /*  portfolio_isotope
-    /*----------------------------------------------------*/
-   
-//	$('.courses_inner').imagesLoaded(function(){
-//        $('.courses_inner').isotope({ 
-//            layoutMode: 'masonry',
-//            percentPosition:true,
-//            masonry: {
-//                columnWidth: 1,
-//            }            
-//        })
-//    });
-	
-	
-	/*----------------------------------------------------*/
-    /*  Clients Slider
-    /*----------------------------------------------------*/
-//    function clients_slider(){
-//        if ( $('.clients_slider').length ){
-//            $('.clients_slider').owlCarousel({
-//                loop:true,
-//                margin: 30,
-//                items: 5,
-//                nav: false,
-//                autoplay: false,
-//                smartSpeed: 1500,
-//                dots:false, 
-//                responsiveClass: true,
-//                responsive: {
-//                    0: {
-//                        items: 1,
-//                    },
-//                    400: {
-//                        items: 2,
-//                    },
-//                    575: {
-//                        items: 3,
-//                    },
-//                    768: {
-//                        items: 4,
-//                    },
-//                    992: {
-//                        items: 5,
-//                    }
-//                }
-//            })
-//        }
-//    }
-//    clients_slider();
-	/*----------------------------------------------------*/
-    /*  MailChimp Slider
-    /*----------------------------------------------------*/
-    function mailChimp(){
-        $('#mc_embed_signup').find('form').ajaxChimp();
+    Build Button Here.
+
+*/
+function themeSwitch() {
+    let dark = document.getElementById("toggle").checked;
+    console.log(dark);
+
+    let background = title = text = card = theme = image = "";
+    if (dark) {
+        /* ==== Light Mode ==== */
+        background = "var(--background-light)";
+        title = "var(--title-light)";
+        text = "var(--text-light)";
+        card = "var(--card-light)";
+        theme = "var(--secondary)";
+        image = "url(static/img/coffeecupblue-bg.jpg)";
     }
-    mailChimp();
-	
-	$('select').niceSelect();
-	
-	/*----------------------------------------------------*/
-    /*  Simple LightBox js
-    /*----------------------------------------------------*/
-    $('.imageGallery1 .light').simpleLightbox();
-	
-	$('.counter').counterUp({
-		delay: 10,
-		time: 1000
-	});
-	
-	
-	$(".skill_main").each(function() {
-        $(this).waypoint(function() {
-            var progressBar = $(".progress-bar");
-            progressBar.each(function(indx){
-                $(this).css("width", $(this).attr("aria-valuenow") + "%")
-            })
-        }, {
-            triggerOnce: true,
-            offset: 'bottom-in-view'
-
-        });
-    });
-	
-	/*----------------------------------------------------*/
-    /*  Isotope Fillter js
-    /*----------------------------------------------------*/
-	function gallery_isotope(){
-        if ( $('.gallery_f_inner').length ){
-            // Activate isotope in container
-			$(".gallery_f_inner").imagesLoaded( function() {
-                $(".gallery_f_inner").isotope({
-                    layoutMode: 'fitRows',
-                    animationOptions: {
-                        duration: 750,
-                        easing: 'linear'
-                    }
-                }); 
-            });
-			
-            // Add isotope click function
-            $(".gallery_filter li").on('click',function(){
-                $(".gallery_filter li").removeClass("active");
-                $(this).addClass("active");
-
-                var selector = $(this).attr("data-filter");
-                $(".gallery_f_inner").isotope({
-                    filter: selector,
-                    animationOptions: {
-                        duration: 450,
-                        easing: "linear",
-                        queue: false,
-                    }
-                });
-                return false;
-            });
-        }
+    else {
+        /* ==== Dark Mode ==== */
+        background = "var(--background-dark)";
+        title = "var(--title-dark)";
+        text = "var(--text-dark)";
+        card = "var(--card-dark)";
+        theme = "var(--primary)";
+        image = "url(static/img/coffeecup-bg.jpg)";
     }
-    gallery_isotope();
-	
-	/*----------------------------------------------------*/
-    /*  Testimonials Slider
-    /*----------------------------------------------------*/
-    function testimonials_slider(){
-        if ( $('.testi_slider').length ){
-            $('.testi_slider').owlCarousel({
-                loop:true,
-                margin: 30,
-                items: 3,
-                nav: false,
-                autoplay: true,
-                smartSpeed: 1500,
-                dots:true, 
-                responsiveClass: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                    },
-                    768: {
-                        items: 3,
-                    },
-                }
-            })
-        }
-    }
-    testimonials_slider();
-	
-	$(document).ready(function() {
-		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-			disableOn: 700,
-			type: 'iframe',
-			mainClass: 'mfp-fade',
-			removalDelay: 160,
-			preloader: false,
 
-			fixedContentPos: false
-		});
-	}); 
-	
-	/*----------------------------------------------------*/
-    /*  Google map js
-    /*----------------------------------------------------*/
-     
-    if ( $('#mapBox').length ){
-        var $lat = $('#mapBox').data('lat');
-        var $lon = $('#mapBox').data('lon');
-        var $zoom = $('#mapBox').data('zoom');
-        var $marker = $('#mapBox').data('marker');
-        var $info = $('#mapBox').data('info');
-        var $markerLat = $('#mapBox').data('mlat');
-        var $markerLon = $('#mapBox').data('mlon');
-        var map = new GMaps({
-        el: '#mapBox',
-        lat: $lat,
-        lng: $lon,
-        scrollwheel: false,
-        scaleControl: true,
-        streetViewControl: false,
-        panControl: true,
-        disableDoubleClickZoom: true,
-        mapTypeControl: false,
-        zoom: $zoom,
-            styles: [
-                {
-                    "featureType": "water",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#dcdfe6"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "transit",
-                    "stylers": [
-                        {
-                            "color": "#808080"
-                        },
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.highway",
-                    "elementType": "geometry.stroke",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "color": "#dcdfe6"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.highway",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#ffffff"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.local",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "color": "#ffffff"
-                        },
-                        {
-                            "weight": 1.8
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.local",
-                    "elementType": "geometry.stroke",
-                    "stylers": [
-                        {
-                            "color": "#d7d7d7"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "poi",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "color": "#ebebeb"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "administrative",
-                    "elementType": "geometry",
-                    "stylers": [
-                        {
-                            "color": "#a7a7a7"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.arterial",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#ffffff"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.arterial",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#ffffff"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "landscape",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "color": "#efefef"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "color": "#696969"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "administrative",
-                    "elementType": "labels.text.fill",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "color": "#737373"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "poi",
-                    "elementType": "labels.icon",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "poi",
-                    "elementType": "labels",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road.arterial",
-                    "elementType": "geometry.stroke",
-                    "stylers": [
-                        {
-                            "color": "#d6d6d6"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "labels.icon",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {},
-                {
-                    "featureType": "poi",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#dadada"
-                        }
-                    ]
-                }
-            ]
-        });
-    }
-	
+    /* ======= Change hero background image ======== */
+    document.getElementsByClassName("bg-image")[0].style.backgroundImage = image;
 
-})(jQuery)
+    /* ======= Change body background and text color ===== */
+    let body = document.getElementsByTagName("body")[0];
+    body.style.background = background;
+    body.style.color = text;
+
+    /* ====== Change background of card boxes ======= */
+    document.getElementsByClassName("box-shadow-full")[0].style.background = card;
+    let work_box = document.getElementsByClassName("work-box");
+    for (let i = 0; i < work_box.length; i++) {
+        work_box[i].style.background = card;
+    }
+
+    /* ====== Change color of titles and subtitles ======== */
+    let w_titles = document.getElementsByClassName("w-title");
+    for (let i = 0; i < w_titles.length; i++) {
+        w_titles[i].style.color = text;
+    }
+    let title_left = document.getElementsByClassName("title-left")[0];
+    title_left.style.color = title;
+    title_left.style.setProperty("--primary", "#0f70b5"); /* <-- pseudo-element */
+    document.getElementsByClassName("skill-mf")[0].getElementsByTagName("span")[0].style.color = text;
+    let subtitle_a = document.getElementsByClassName("subtitle-a");
+    for (let i = 0; i < subtitle_a.length; i++) {
+        subtitle_a[i].style.color = text;
+    }
+    let title_s = document.getElementsByClassName("title-s");
+    for (let i = 0; i < title_s.length; i++) {
+        title_s[i].style.color = title;
+    }
+
+    /* ======= Change theme of overlay, lines, and buttons background ======= */
+    document.getElementsByClassName("overlay-mf")[0].style.background = theme;
+    let line_mf = document.getElementsByClassName("line-mf");
+    for (let i = 0; i < line_mf.length; i++) {
+        line_mf[i].style.background = theme;
+    }
+    document.getElementsByClassName("back-to-top")[0].style.background = theme;
+
+    /* ====== Portfolio category and links ======= */
+    let w_category = document.getElementsByClassName("w-ctegory");
+    for (let i = 0; i < w_category.length; i++) {
+        w_category[i].style.color = theme;
+    }
+    let links = document.getElementsByClassName("w-like");
+    for (let i = 0; i < links.length; i++) {
+        links[i].getElementsByTagName("a")[0].style.color = theme;
+    }
+
+    /* ====== Change About social icons background ===== */
+    let social_button = document.getElementsByClassName("personal_social")[0].getElementsByTagName("a");
+    for (let i = 0; i < social_button.length; i++) {
+        social_button[i].style.background = theme;
+    }
+
+}
